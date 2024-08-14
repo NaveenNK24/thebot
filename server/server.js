9309f9d76ef5f4b8d3d3e30a123427395daa8d8f
NaveenNK24 <naveensaven@gmail.com>
2024-08-14 20:07:30 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -18,5 +18,5 @@ app.use('/api', userRoutes);
 connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
