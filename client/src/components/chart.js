481d4c6a25afc68180db83a83113a7238f1b09d9
NaveenNK24 <naveensaven@gmail.com>
2024-08-19 20:22:21 +0530

    vercel deployed

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -1,5 +1,4 @@
-import React, {  useEffect } from 'react';
-// import { chartReducer, initialState } from '../reducers/chartReducer.js';
+import React, { useEffect, useRef } from 'react';
 import { fetchData } from '../actions/chartAction.js';
 import { createChart } from 'lightweight-charts';
 import { useParams } from 'react-router-dom';
@@ -10,33 +9,49 @@ const ChartComponent1 = () => {
   const { symbol } = useParams(); 
   const { data, loading, error } = useSelector((state) => state.data);
 
+  // Create a ref to store the chart instance
+  const chartRef = useRef(null);
+
   useEffect(() => {
     if (symbol) {
         dispatch(fetchData(symbol));
     }
-}, [dispatch, symbol]);
+  }, [dispatch, symbol]);
 
   useEffect(() => {
     if (data.length > 0) {
-      const chartProperties = {
-        timeScale: {
-          timeVisible: true,
-          secondsVisible: true,
-        },
-      };
+      // Only create the chart if it hasn't been created yet
+      if (!chartRef.current) {
+        const chartProperties = {
+          timeScale: {
+            timeVisible: true,
+            secondsVisible: true,
+          },
+        };
 
-      const domElement = document.getElementById('tvchart');
-      const chart = createChart(domElement, chartProperties);
-      const candleSeries = chart.addCandlestickSeries();
-      candleSeries.setData(data);
+        const domElement = document.getElementById('tvchart');
+        const chart = createChart(domElement, chartProperties);
+        chartRef.current = chart;
 
-      const sma_series = chart.addLineSeries({ color: 'red', lineWidth: 1 });
+        const candleSeries = chart.addCandlestickSeries();
+        candleSeries.setData(data);
+
+        const sma_series = chart.addLineSeries({ color: 'red', lineWidth: 1 });
         const sma_data = data
-        .filter((d) => d.sma)
-        .map((d) => ({ time: d.time, value: d.sma }));
+          .filter((d) => d.sma)
+          .map((d) => ({ time: d.time, value: d.sma }));
         sma_series.setData(sma_data);
+      } else {
+        // Update the chart data if the chart already exists
+        const candleSeries = chartRef.current.addCandlestickSeries();
+        candleSeries.setData(data);
 
-
+        const sma_series = chartRef.current.addLineSeries({ color: 'red', lineWidth: 1 });
+        const sma_data = data
+          .filter((d) => d.sma)
+          .map((d) => ({ time: d.time, value: d.sma }));
+        sma_series.setData(sma_data);
+      }
     }
   }, [data]);
 
